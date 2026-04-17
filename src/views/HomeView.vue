30dd33a1d6b29487235bb7da263ed1f77ceeab5c
Raflixyz <digitalpay011@gmail.com>
2026-04-17 11:22:33 +0700

    Add src/views/HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HeroSection from '@/components/sections/HeroSection.vue'
+import ServicesSection from '@/components/sections/ServicesSection.vue'
+import WhyUsSection from '@/components/sections/WhyUsSection.vue'
+import HowItWorksSection from '@/components/sections/HowItWorksSection.vue'
+import TestimonialsSection from '@/components/sections/TestimonialsSection.vue'
+import FaqSection from '@/components/sections/FaqSection.vue'
+import CtaSection from '@/components/sections/CtaSection.vue'
+</script>
+
+<template>
+  <div>
+    <HeroSection />
+    <ServicesSection />
+    <HowItWorksSection />
+    <WhyUsSection />
+    <TestimonialsSection />
+    <FaqSection />
+    <CtaSection />
+  </div>
+</template>
